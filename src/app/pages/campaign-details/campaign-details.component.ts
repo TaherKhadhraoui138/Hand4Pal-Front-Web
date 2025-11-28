f43167b08d5a436f50ef0cfc745188b7d2bd1ecc
TaherKhadhraoui138 <medtaher.khadhraoui@bizerte.r-iset.tn>
2025-11-28 18:39:54 +0100

    design

--- src/app/pages/campaign-details/campaign-details.component.ts
+++ src/app/pages/campaign-details/campaign-details.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { DonationModalComponent } from '../../shared/donation-modal/donation-modal.component';
+import { CommentComponent } from '../../shared/comment/comment.component';
 
 @Component({
   selector: 'app-campaign-details',
   standalone: true,
-  imports: [RouterLink, CommonModule, DonationModalComponent],
+  imports: [RouterLink, CommonModule, DonationModalComponent, CommentComponent],
   templateUrl: './campaign-details.component.html',
   styleUrls: ['./campaign-details.component.css']
 })
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent { }
+export class RegisterComponent {
+  userType: 'citizen' | 'association' = 'citizen';
+
+  selectUserType(type: 'citizen' | 'association') {
+    this.userType = type;
+  }
+}
